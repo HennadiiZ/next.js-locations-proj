5d153ec82be3ae801480143e395ab0fd63ac465a
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-01-27 14:37:28 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/05/c1/ff/28/caption.jpg?w=1400&h=900&s=1',
+    title: 'Mountains in Canada',
+    address: 'Alberta',
+    descriprion: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/0c/ec/48/yukon-northern-lights.jpg?w=1400&h=-1&s=1',
+    title: 'Aurora in Canada',
+    address: 'Alberta',
+    descriprion: 'This is a second meetup!',
+  },
+  {
+    id: 'm3',
+    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/b9/c1/75/worth-a-detour-of-the.jpg?w=2000&h=-1&s=1',
+    title: 'Lakes in Canada',
+    address: 'Alberta',
+    descriprion: 'This is a third meetup!',
+  },
+];
+
+function HomePage() {
+  return (
+    <>
+      <MeetupList meetups={DUMMY_MEETUPS}/>
+    </>
+  )
+};
+export default HomePage;
