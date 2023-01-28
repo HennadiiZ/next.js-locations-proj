adeb20e998aa911a3bec19eeeec4d63b99d49cc1
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-01-28 18:06:35 +0200

    Working with MongoDB

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a36b01e2b078702b2c54";
+/******/ 	var hotCurrentHash = "75dc59c6f6e731916f45";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,13 +1,28 @@
 //   /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
     const { title, image, address, description } = data;
 
+    // MongoClient.connect('mongodb+srv://12345:12345@cluster0.dclqwcf.mongodb.net/?retryWrites=true&w=majority');
+    const client = await MongoClient.connect('mongodb+srv://12345:12345@cluster0.dclqwcf.mongodb.net/meetups?retryWrites=true&w=majority');
+    const db = client.db();
 
+    const meetupsCollection = db.collection('meetups');
+
+    // meetupsCollection.insertOne({ title, image, address, description });
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({message: 'Meetup inserted!'});
   }
 };
 
